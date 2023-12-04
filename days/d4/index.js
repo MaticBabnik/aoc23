2924718141ba8ecd772f2906b8c9a02e3103bc34
Matic Babnik <matic.babnik@outlook.com>
2023-12-04 22:19:46 +0100

    day 4 nice

--- days/d4/index.js
+++ days/d4/index.js
@@ -1,17 +1,12 @@
 import { input } from "../unsuck.js";
 
-const inp = input("input", "\n").map(x => x.split(':')[1].split('|').map(x => x.trim().split(/\s+/g)));
+const p = input("inputt", "\n")
+    .map(x => x.split(':')[1].split('|').map(x => x.trim().split(/\s+/g)))
+    .map(([h, w]) => (h.filter(t => w.includes(t))).length)
 
 console.log(
-    inp.map(([h, w]) => (1 << (h.filter(t => w.includes(t))).length) >> 1).sum()
+    p.map(x => (1 << x >> 1)).sum(),
+    p.map(_ => 1).map((c, i, a) =>
+        [...Array(p[i])].forEach((_, j) => a[i + 1 + j] += c) || c
+    ).sum()
 )
-
-const cards = inp.map(([h, w]) => (h.filter(t => w.includes(t))).length);
-const c2 = cards.map(x => 1);
-
-for (let i = 0; i < cards.length; i++) {
-    for (let j = i + 1; j <= i + cards[i] && j < cards.length; j++) {
-        c2[j] += c2[i];
-    }
-}
-console.log(c2.sum())
