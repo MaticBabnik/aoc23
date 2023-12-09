2f9b7772a5ee0c4101f5dc8e744fc1048a657cfb
Matic Babnik <matic.babnik@outlook.com>
2023-12-09 06:31:13 +0100

    D9 cleanup 🧹

--- days/d9/index.js
+++ days/d9/index.js
@@ -1,13 +1,6 @@
 import { input } from "../unsuck.js";
 
-const p = input("input", "\n").map(x => x.split(" ").map(y => y - 0));
+const p = input("input", "\n").map(x => x.split(" ").map(y => y - 0)),
+    f = (x, _r = [], _s = 0) => (x.reduce((p, c) => (_s += _r.push(c - p), c)), x.at(-1) + (_s == 0 ? 0 : f(_r)))
 
-function p1(x) {
-    const r = [];
-    let sum = 0;
-    x.reduce((p, c) => (sum += r.push(c - p), c))
-    if (sum == 0) return [];
-    return [x.at(-1), ...p1(r)]
-}
-
-console.log(p.map(x => p1(x).sum()).sum(), p.map(x => p1(x.reverse()).sum()).sum())
+console.log(p.map(x => f(x)).sum(), p.map(x => f(x.toReversed())).sum())
